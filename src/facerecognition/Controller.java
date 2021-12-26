f47ebcb12794e20e7176d8bc6f960962929e6c05
amruthn1 <amruthnadimpally1@gmail.com>
2021-12-26 15:33:37 -0500

    Added file browser and updated facial recognition model

--- src/facerecognition/Controller.java
+++ src/facerecognition/Controller.java
@@ -3,6 +3,8 @@ package facerecognition;
 import facerecognition.utils.Utils;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.FileChooser;
@@ -14,17 +16,40 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+
 public class Controller {
+    @FXML
+    private TextField fpsCounter = new TextField();
     @FXML
     private Button button;
     @FXML
     private ImageView usercam;
     private boolean isActive = false;
+    private boolean shouldOverlay = false;
     private VideoCapture uCam = new VideoCapture();
     private ScheduledExecutorService timer;
+    private String filePath;
     FileChooser fileChooser = new FileChooser();
     @FXML
+    private CheckBox imgoverlaycheckbox = new CheckBox();
+    @FXML
+    protected void imgoverlay(javafx.event.ActionEvent actionEvent) {
+        if (shouldOverlay) {
+            shouldOverlay = false;
+        } else {
+            shouldOverlay = true;
+        }
+    }
+    @FXML
     protected void startcam(javafx.event.ActionEvent actionEvent) {
+        if (shouldOverlay) {
+            FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png");
+            fileChooser.getExtensionFilters().add(imageFilter);
+            File selectedFile = fileChooser.showOpenDialog(null);
+            filePath = selectedFile.getPath();
+        } else {
+            filePath = "null";
+        }
         if (!isActive) {
             uCam.open(0);
             if (uCam.isOpened()){
@@ -38,13 +63,12 @@ public class Controller {
                         } catch (Exception c) {
                             System.err.println(c);
                         }
-                        Image img = Utils.mat2Image(FacialRecog.main(frame)); //frame
+                        Image img = Utils.mat2Image(FacialRecog.main(frame, filePath)); //frame
                         Utils.onFXThread(usercam.imageProperty(), img);
                     }
                 };
                 this.timer = Executors.newSingleThreadScheduledExecutor();
                 this.timer.scheduleAtFixedRate(getFrames, 0, 33, TimeUnit.MILLISECONDS);
-
             }
         }
     }
--- src/facerecognition/FacialRecog.java
+++ src/facerecognition/FacialRecog.java
@@ -8,15 +8,18 @@ import org.opencv.objdetect.Objdetect;
 
 public class FacialRecog {
     protected int absoluteFaceSize = 0;
+    //Model from OpenCV project
     private CascadeClassifier faceCascade = new CascadeClassifier("src/facerecognition/model.xml");
     MatOfRect faces = new MatOfRect();
-    public static Mat main(Mat frame) {
+    public static Mat main(Mat frame, String filePath) {
         FacialRecog fr = new FacialRecog();
-        return fr.process(frame);
+        return fr.process(frame, filePath);
     }
-    private Mat process(Mat frame){
-        Mat output = new Mat();
-        Mat overlay = Imgcodecs.imread("src/facerecognition/pig.png");
+    private Mat process(Mat frame, String filePath){
+        Mat overlay = new Mat();
+        if (filePath != "null") {
+            overlay = Imgcodecs.imread(filePath);
+        }
         Mat grayFrame = new Mat();
         Imgproc.cvtColor(frame, grayFrame, Imgproc.COLOR_BGR2GRAY);
         Imgproc.equalizeHist(grayFrame, grayFrame);
@@ -32,10 +35,12 @@ public class FacialRecog {
         for (int j = 0; j < facesArr.length; j++) {
             Imgproc.rectangle(frame, facesArr[j].tl(), facesArr[j].br(), new Scalar(0, 0, 255), 3);
             //Overlay Image Code
-            rect = new Rect(facesArr[j].tl(), facesArr[j].br());
-            Imgproc.resize(overlay, overlay, rect.size());
-            Mat submat = frame.submat(new Rect(rect.x, rect.y, overlay.cols(), overlay.rows()));
-            overlay.copyTo(submat);
+            if (filePath != "null") {
+                rect = new Rect(facesArr[j].tl(), facesArr[j].br());
+                Imgproc.resize(overlay, overlay, rect.size());
+                Mat submat = frame.submat(new Rect(rect.x, rect.y, overlay.cols(), overlay.rows()));
+                overlay.copyTo(submat);
+            }
         }
         return frame;
     }
